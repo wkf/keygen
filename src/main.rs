29b75e2322313b95101cf7d340b13ffbf1ffe1c9
Will Farrell <wkfarrell@gmail.com>
2021-03-26 12:10:10 -0400

    Get working on latest nightly.

--- src/main.rs
+++ src/main.rs
@@ -1,202 +1,222 @@
-#![feature(linked_list_extras)]
+#![feature(linked_list_cursors)]
 
+mod annealing;
 mod layout;
 mod penalty;
-mod annealing;
 mod simulator;
 
 extern crate getopts;
 
+use getopts::Options;
 use std::env;
 use std::fs::File;
 use std::io::Read;
-use getopts::Options;
 
-fn main()
-{
-	let mut opts = Options::new();
-	opts.optflag("h", "help", "print this help menu");
-	opts.optflag("d", "debug", "show debug logging");
-	opts.optopt("t", "top", "number of top layouts to print (default: 1)", "TOP_LAYOUTS");
-	opts.optopt("s", "swaps-per-iteration", "maximum number of swaps per iteration (default: 3)", "SWAPS");
+fn main() {
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("d", "debug", "show debug logging");
+    opts.optopt(
+        "t",
+        "top",
+        "number of top layouts to print (default: 1)",
+        "TOP_LAYOUTS",
+    );
+    opts.optopt(
+        "s",
+        "swaps-per-iteration",
+        "maximum number of swaps per iteration (default: 3)",
+        "SWAPS",
+    );
 
-	let args: Vec<String> = env::args().collect();
-	let progname = &args[0];
-	if args.len() < 2 {
-		print_usage(progname, opts);
-		return;
-	}
-	let command = &args[1];
-	let matches = match opts.parse(&args[2..]) {
-		Ok(m) => { m }
-		Err(f) => { panic!(f.to_string()) }
-	};
+    let args: Vec<String> = env::args().collect();
+    let progname = &args[0];
+    if args.len() < 2 {
+        print_usage(progname, opts);
+        return;
+    }
+    let command = &args[1];
+    let matches = match opts.parse(&args[2..]) {
+        Ok(m) => m,
+        Err(f) => {
+            panic!(f.to_string())
+        }
+    };
 
-	// --help
-	if matches.opt_present("h") {
-		print_usage(progname, opts);
-		return;
-	}
+    // --help
+    if matches.opt_present("h") {
+        print_usage(progname, opts);
+        return;
+    }
 
-	// Read corpus.
-	let corpus_filename = match matches.free.get(0) {
-		Some(f) => f,
-		None => {
-			print_usage(progname, opts);
-			return;
-		},
-	};
-	let mut f = match File::open(corpus_filename) {
-		Ok(f) => f,
-		Err(e) => {
-			println!("Error: {}", e);
-			panic!("could not read corpus");
-		},
-	};
-	let mut corpus = String::new();
-	match f.read_to_string(&mut corpus) {
-		Ok(_) => (),
-		Err(e) => {
-			println!("Error: {}", e);
-			panic!("could not read corpus");
-		}
-	};
+    // Read corpus.
+    let corpus_filename = match matches.free.get(0) {
+        Some(f) => f,
+        None => {
+            print_usage(progname, opts);
+            return;
+        }
+    };
+    let mut f = match File::open(corpus_filename) {
+        Ok(f) => f,
+        Err(e) => {
+            println!("Error: {}", e);
+            panic!("could not read corpus");
+        }
+    };
+    let mut corpus = String::new();
+    match f.read_to_string(&mut corpus) {
+        Ok(_) => (),
+        Err(e) => {
+            println!("Error: {}", e);
+            panic!("could not read corpus");
+        }
+    };
 
-	// Read layout, if applicable.
-	let _layout;
-	let layout = match matches.free.get(1) {
-		None => &layout::INIT_LAYOUT,
-		Some(layout_filename) => {
-			let mut f = match File::open(layout_filename) {
-				Ok(f) => f,
-				Err(e) => {
-					println!("Error: {}", e);
-					panic!("could not read layout");
-				}
-			};
-			let mut layout_str = String::new();
-			match f.read_to_string(&mut layout_str) {
-				Ok(_) => (),
-				Err(e) => {
-					println!("Error: {}", e);
-					panic!("could not read layout");
-				}
-			};
-			_layout = layout::Layout::from_string(&layout_str[..]);
-			&_layout
-		},
-	};
+    // Read layout, if applicable.
+    let _layout;
+    let layout = match matches.free.get(1) {
+        None => &layout::INIT_LAYOUT,
+        Some(layout_filename) => {
+            let mut f = match File::open(layout_filename) {
+                Ok(f) => f,
+                Err(e) => {
+                    println!("Error: {}", e);
+                    panic!("could not read layout");
+                }
+            };
+            let mut layout_str = String::new();
+            match f.read_to_string(&mut layout_str) {
+                Ok(_) => (),
+                Err(e) => {
+                    println!("Error: {}", e);
+                    panic!("could not read layout");
+                }
+            };
+            _layout = layout::Layout::from_string(&layout_str[..]);
+            &_layout
+        }
+    };
 
-	// Parse options.
-	let debug = matches.opt_present("d");
-	let top   = numopt(matches.opt_str("t"), 1usize);
-	let swaps = numopt(matches.opt_str("s"), 3usize);
+    // Parse options.
+    let debug = matches.opt_present("d");
+    let top = numopt(matches.opt_str("t"), 1usize);
+    let swaps = numopt(matches.opt_str("s"), 3usize);
 
-	match command.as_ref() {
-		"run" => run(&corpus[..], layout, debug, top, swaps),
-		"run-ref" => run_ref(&corpus[..]),
-		"refine" => refine(&corpus[..], layout, debug, top, swaps),
-		_ => print_usage(progname, opts),
-	};
+    match command.as_ref() {
+        "run" => run(&corpus[..], layout, debug, top, swaps),
+        "run-ref" => run_ref(&corpus[..]),
+        "refine" => refine(&corpus[..], layout, debug, top, swaps),
+        _ => print_usage(progname, opts),
+    };
 }
 
-fn run(s: &str, layout: &layout::Layout, debug: bool, top: usize, swaps: usize)
-{
-	let penalties = penalty::init();
-	let init_pos_map = layout::INIT_LAYOUT.get_position_map();
-	let quartads = penalty::prepare_quartad_list(s, &init_pos_map);
-	let len = s.len();
+fn run(s: &str, layout: &layout::Layout, debug: bool, top: usize, swaps: usize) {
+    let penalties = penalty::init();
+    let init_pos_map = layout::INIT_LAYOUT.get_position_map();
+    let quartads = penalty::prepare_quartad_list(s, &init_pos_map);
+    let len = s.len();
 
-	loop {
-		simulator::simulate(&quartads, len, layout, &penalties, debug, top, swaps);
-	}
+    loop {
+        simulator::simulate(&quartads, len, layout, &penalties, debug, top, swaps);
+    }
 }
 
-fn run_ref(s: &str)
-{
-	let penalties = penalty::init();
-	let init_pos_map = layout::INIT_LAYOUT.get_position_map();
-	let quartads = penalty::prepare_quartad_list(s, &init_pos_map);
-	let len = s.len();
+fn run_ref(s: &str) {
+    let penalties = penalty::init();
+    let init_pos_map = layout::INIT_LAYOUT.get_position_map();
+    let quartads = penalty::prepare_quartad_list(s, &init_pos_map);
+    let len = s.len();
 
-	let penalty = penalty::calculate_penalty(&quartads, len, &layout::QWERTY_LAYOUT, &penalties, true);
-	println!("Reference: QWERTY");
-	simulator::print_result(&layout::QWERTY_LAYOUT, &penalty);
-	println!("");
+    let penalty =
+        penalty::calculate_penalty(&quartads, len, &layout::QWERTY_LAYOUT, &penalties, true);
+    println!("Reference: QWERTY");
+    simulator::print_result(&layout::QWERTY_LAYOUT, &penalty);
+    println!("");
 
-	let penalty = penalty::calculate_penalty(&quartads, len, &layout::DVORAK_LAYOUT, &penalties, true);
-	println!("Reference: DVORAK");
-	simulator::print_result(&layout::DVORAK_LAYOUT, &penalty);
-	println!("");
+    let penalty =
+        penalty::calculate_penalty(&quartads, len, &layout::DVORAK_LAYOUT, &penalties, true);
+    println!("Reference: DVORAK");
+    simulator::print_result(&layout::DVORAK_LAYOUT, &penalty);
+    println!("");
 
-	let penalty = penalty::calculate_penalty(&quartads, len, &layout::COLEMAK_LAYOUT, &penalties, true);
-	println!("Reference: COLEMAK");
-	simulator::print_result(&layout::COLEMAK_LAYOUT, &penalty);
-	println!("");
+    let penalty =
+        penalty::calculate_penalty(&quartads, len, &layout::COLEMAK_LAYOUT, &penalties, true);
+    println!("Reference: COLEMAK");
+    simulator::print_result(&layout::COLEMAK_LAYOUT, &penalty);
+    println!("");
 
-	let penalty = penalty::calculate_penalty(&quartads, len, &layout::QGMLWY_LAYOUT, &penalties, true);
-	println!("Reference: QGMLWY");
-	simulator::print_result(&layout::QGMLWY_LAYOUT, &penalty);
-	println!("");
+    let penalty =
+        penalty::calculate_penalty(&quartads, len, &layout::QGMLWY_LAYOUT, &penalties, true);
+    println!("Reference: QGMLWY");
+    simulator::print_result(&layout::QGMLWY_LAYOUT, &penalty);
+    println!("");
 
-	let penalty = penalty::calculate_penalty(&quartads, len, &layout::WORKMAN_LAYOUT, &penalties, true);
-	println!("Reference: WORKMAN");
-	simulator::print_result(&layout::WORKMAN_LAYOUT, &penalty);
-	println!("");
+    let penalty =
+        penalty::calculate_penalty(&quartads, len, &layout::WORKMAN_LAYOUT, &penalties, true);
+    println!("Reference: WORKMAN");
+    simulator::print_result(&layout::WORKMAN_LAYOUT, &penalty);
+    println!("");
 
-	let penalty = penalty::calculate_penalty(&quartads, len, &layout::MALTRON_LAYOUT, &penalties, true);
-	println!("Reference: MALTRON");
-	simulator::print_result(&layout::MALTRON_LAYOUT, &penalty);
-	println!("");
+    let penalty =
+        penalty::calculate_penalty(&quartads, len, &layout::MALTRON_LAYOUT, &penalties, true);
+    println!("Reference: MALTRON");
+    simulator::print_result(&layout::MALTRON_LAYOUT, &penalty);
+    println!("");
 
-	let penalty = penalty::calculate_penalty(&quartads, len, &layout::MTGAP_LAYOUT, &penalties, true);
-	println!("Reference: MTGAP");
-	simulator::print_result(&layout::MTGAP_LAYOUT, &penalty);
-	println!("");
+    let penalty =
+        penalty::calculate_penalty(&quartads, len, &layout::MTGAP_LAYOUT, &penalties, true);
+    println!("Reference: MTGAP");
+    simulator::print_result(&layout::MTGAP_LAYOUT, &penalty);
+    println!("");
 
-	let penalty = penalty::calculate_penalty(&quartads, len, &layout::CAPEWELL_LAYOUT, &penalties, true);
-	println!("Reference: CAPEWELL");
-	simulator::print_result(&layout::CAPEWELL_LAYOUT, &penalty);
-	println!("");
+    let penalty =
+        penalty::calculate_penalty(&quartads, len, &layout::CAPEWELL_LAYOUT, &penalties, true);
+    println!("Reference: CAPEWELL");
+    simulator::print_result(&layout::CAPEWELL_LAYOUT, &penalty);
+    println!("");
 
-	let penalty = penalty::calculate_penalty(&quartads, len, &layout::ARENSITO_LAYOUT, &penalties, true);
-	println!("Reference: ARENSITO");
-	simulator::print_result(&layout::ARENSITO_LAYOUT, &penalty);
-	println!("");
+    let penalty =
+        penalty::calculate_penalty(&quartads, len, &layout::ARENSITO_LAYOUT, &penalties, true);
+    println!("Reference: ARENSITO");
+    simulator::print_result(&layout::ARENSITO_LAYOUT, &penalty);
+    println!("");
 
-	let penalty = penalty::calculate_penalty(&quartads, len, &layout::INIT_LAYOUT, &penalties, true);
-	println!("Reference: INITIAL");
-	simulator::print_result(&layout::INIT_LAYOUT, &penalty);
+    let penalty =
+        penalty::calculate_penalty(&quartads, len, &layout::INIT_LAYOUT, &penalties, true);
+    println!("Reference: INITIAL");
+    simulator::print_result(&layout::INIT_LAYOUT, &penalty);
 }
 
-fn refine(s: &str, layout: &layout::Layout, debug: bool, top: usize, swaps: usize)
-{
-	let penalties = penalty::init();
-	let init_pos_map = layout::INIT_LAYOUT.get_position_map();
-	let quartads = penalty::prepare_quartad_list(s, &init_pos_map);
-	let len = s.len();
+fn refine(s: &str, layout: &layout::Layout, debug: bool, top: usize, swaps: usize) {
+    let penalties = penalty::init();
+    let init_pos_map = layout::INIT_LAYOUT.get_position_map();
+    let quartads = penalty::prepare_quartad_list(s, &init_pos_map);
+    let len = s.len();
 
-	simulator::refine(&quartads, len, layout, &penalties, debug, top, swaps);
+    simulator::refine(&quartads, len, layout, &penalties, debug, top, swaps);
 }
 
-fn print_usage(progname: &String, opts: Options)
-{
-	let brief = format!("Usage: {} (run|run-ref) <corpus> [OPTIONS]", progname);
-	print!("{}", opts.usage(&brief));
+fn print_usage(progname: &String, opts: Options) {
+    let brief = format!("Usage: {} (run|run-ref) <corpus> [OPTIONS]", progname);
+    print!("{}", opts.usage(&brief));
 }
 
-fn numopt<T>(s: Option<String>, default: T)
--> T
-where T: std::str::FromStr + std::fmt::Display
+fn numopt<T>(s: Option<String>, default: T) -> T
+where
+    T: std::str::FromStr + std::fmt::Display,
 {
-	match s {
-		None => default,
-		Some(num) => match num.parse::<T>() {
-			Ok(n) => n,
-			Err(_) => {
-				println!("Error: invalid option value {}. Using default value {}.", num, default);
-				default
-			},
-		},
-	}
+    match s {
+        None => default,
+        Some(num) => match num.parse::<T>() {
+            Ok(n) => n,
+            Err(_) => {
+                println!(
+                    "Error: invalid option value {}. Using default value {}.",
+                    num, default
+                );
+                default
+            }
+        },
+    }
 }
--- src/simulator.rs
+++ src/simulator.rs
@@ -1,210 +1,205 @@
 /// Applies the math in annealing.rs to keyboard layouts.
-
-
 extern crate rand;
 
 use self::rand::random;
 use std::cmp::Ordering;
 use std::collections::LinkedList;
 
+use annealing;
 use layout;
 use penalty;
-use annealing;
 
-struct BestLayoutsEntry
-{
-	layout:  layout::Layout,
-	penalty: f64,
+struct BestLayoutsEntry {
+    layout: layout::Layout,
+    penalty: f64,
 }
 
-impl BestLayoutsEntry
-{
-	fn cmp(&self, other: &BestLayoutsEntry)
-	-> Ordering
-	{
-		match self.penalty.partial_cmp(&other.penalty) {
-			Some(ord) => ord,
-			None => Ordering::Equal
-		}
-	}
+impl BestLayoutsEntry {
+    fn cmp(&self, other: &BestLayoutsEntry) -> Ordering {
+        match self.penalty.partial_cmp(&other.penalty) {
+            Some(ord) => ord,
+            None => Ordering::Equal,
+        }
+    }
 }
 
 pub fn simulate<'a>(
-	quartads:    &penalty::QuartadList<'a>,
-	len:          usize,
-	init_layout: &layout::Layout,
-	penalties:   &Vec<penalty::KeyPenalty<'a>>,
-	debug:        bool,
-	top_layouts:  usize,
-	num_swaps:    usize)
-{
-	let penalty = penalty::calculate_penalty(&quartads, len, init_layout, penalties, true);
+    quartads: &penalty::QuartadList<'a>,
+    len: usize,
+    init_layout: &layout::Layout,
+    penalties: &Vec<penalty::KeyPenalty<'a>>,
+    debug: bool,
+    top_layouts: usize,
+    num_swaps: usize,
+) {
+    let penalty = penalty::calculate_penalty(&quartads, len, init_layout, penalties, true);
 
-	if debug {
-		println!("Initial layout:");
-		print_result(init_layout, &penalty);
-	}
+    if debug {
+        println!("Initial layout:");
+        print_result(init_layout, &penalty);
+    }
 
-	// Keep track of the best layouts we've encountered.
-	let mut best_layouts: LinkedList<BestLayoutsEntry> = LinkedList::new();
+    // Keep track of the best layouts we've encountered.
+    let mut best_layouts: LinkedList<BestLayoutsEntry> = LinkedList::new();
 
-	let mut accepted_layout = init_layout.clone();
-	let mut accepted_penalty = penalty.1;
-	for i in annealing::get_simulation_range() {
-		// Copy and shuffle this iteration of the layout.
-		let mut curr_layout = accepted_layout.clone();
-		curr_layout.shuffle(random::<usize>() % num_swaps + 1);
+    let mut accepted_layout = init_layout.clone();
+    let mut accepted_penalty = penalty.1;
+    for i in annealing::get_simulation_range() {
+        // Copy and shuffle this iteration of the layout.
+        let mut curr_layout = accepted_layout.clone();
+        curr_layout.shuffle(random::<usize>() % num_swaps + 1);
 
-		// Calculate penalty.
-		let curr_layout_copy = curr_layout.clone();
-		let penalty = penalty::calculate_penalty(&quartads, len, &curr_layout, penalties, false);
-		let scaled_penalty = penalty.1;
+        // Calculate penalty.
+        let curr_layout_copy = curr_layout.clone();
+        let penalty = penalty::calculate_penalty(&quartads, len, &curr_layout, penalties, false);
+        let scaled_penalty = penalty.1;
 
-		// Probabilistically accept worse transitions; always accept better
-		// transitions.
-		if annealing::accept_transition(scaled_penalty - accepted_penalty, i) {
-			if debug {
-				println!("Iteration {} accepted with penalty {}", i, scaled_penalty);
-			}
+        // Probabilistically accept worse transitions; always accept better
+        // transitions.
+        if annealing::accept_transition(scaled_penalty - accepted_penalty, i) {
+            if debug {
+                println!("Iteration {} accepted with penalty {}", i, scaled_penalty);
+            }
 
-			accepted_layout = curr_layout_copy.clone();
-			accepted_penalty = scaled_penalty;
+            accepted_layout = curr_layout_copy.clone();
+            accepted_penalty = scaled_penalty;
 
-			// Insert this layout into best layouts.
-			let new_entry = BestLayoutsEntry {
-				layout: curr_layout_copy,
-				penalty: penalty.1,
-			};
-			best_layouts = list_insert_ordered(best_layouts, new_entry);
+            // Insert this layout into best layouts.
+            let new_entry = BestLayoutsEntry {
+                layout: curr_layout_copy,
+                penalty: penalty.1,
+            };
+            best_layouts = list_insert_ordered(best_layouts, new_entry);
 
-			// Limit best layouts list length.
-			while best_layouts.len() > top_layouts {
-				best_layouts.pop_back();
-			}
-		}
-	}
+            // Limit best layouts list length.
+            while best_layouts.len() > top_layouts {
+                best_layouts.pop_back();
+            }
+        }
+    }
 
-	for entry in best_layouts.into_iter() {
-		let layout = entry.layout;
-		let penalty = penalty::calculate_penalty(&quartads, len, &layout, penalties, true);
-		println!("");
-		print_result(&layout, &penalty);
-	}
+    for entry in best_layouts.into_iter() {
+        let layout = entry.layout;
+        let penalty = penalty::calculate_penalty(&quartads, len, &layout, penalties, true);
+        println!("");
+        print_result(&layout, &penalty);
+    }
 }
 
 pub fn refine<'a>(
-	quartads:    &penalty::QuartadList<'a>,
-	len:          usize,
-	init_layout: &layout::Layout,
-	penalties:   &Vec<penalty::KeyPenalty<'a>>,
-	debug:        bool,
-	top_layouts:  usize,
-	num_swaps:    usize)
-{
-	let penalty = penalty::calculate_penalty(&quartads, len, init_layout, penalties, true);
+    quartads: &penalty::QuartadList<'a>,
+    len: usize,
+    init_layout: &layout::Layout,
+    penalties: &Vec<penalty::KeyPenalty<'a>>,
+    debug: bool,
+    top_layouts: usize,
+    num_swaps: usize,
+) {
+    let penalty = penalty::calculate_penalty(&quartads, len, init_layout, penalties, true);
 
-	println!("Initial layout:");
-	print_result(init_layout, &penalty);
+    println!("Initial layout:");
+    print_result(init_layout, &penalty);
 
-	let mut curr_layout = init_layout.clone();
-	let mut curr_penalty = penalty.1;
+    let mut curr_layout = init_layout.clone();
+    let mut curr_penalty = penalty.1;
 
-	loop {
-		// Test every layout within `num_swaps` swaps of the initial layout.
-		let mut best_layouts: LinkedList<BestLayoutsEntry> = LinkedList::new();
-		let permutations = layout::LayoutPermutations::new(init_layout, num_swaps);
-		for (i, layout) in permutations.enumerate() {
-			let penalty = penalty::calculate_penalty(&quartads, len, &layout, penalties, false);
+    loop {
+        // Test every layout within `num_swaps` swaps of the initial layout.
+        let mut best_layouts: LinkedList<BestLayoutsEntry> = LinkedList::new();
+        let permutations = layout::LayoutPermutations::new(init_layout, num_swaps);
+        for (i, layout) in permutations.enumerate() {
+            let penalty = penalty::calculate_penalty(&quartads, len, &layout, penalties, false);
 
-			if debug {
-				println!("Iteration {}: {}", i, penalty.1);
-			}
+            if debug {
+                println!("Iteration {}: {}", i, penalty.1);
+            }
 
-			// Insert this layout into best layouts.
-			let new_entry = BestLayoutsEntry {
-				layout: layout,
-				penalty: penalty.1,
-			};
-			best_layouts = list_insert_ordered(best_layouts, new_entry);
+            // Insert this layout into best layouts.
+            let new_entry = BestLayoutsEntry {
+                layout: layout,
+                penalty: penalty.1,
+            };
+            best_layouts = list_insert_ordered(best_layouts, new_entry);
 
-			// Limit best layouts list length.
-			while best_layouts.len() > top_layouts {
-				best_layouts.pop_back();
-			}
-		}
+            // Limit best layouts list length.
+            while best_layouts.len() > top_layouts {
+                best_layouts.pop_back();
+            }
+        }
 
-		// Print the top layouts.
-		for entry in best_layouts.iter() {
-			let ref layout = entry.layout;
-			let penalty = penalty::calculate_penalty(&quartads, len, &layout, penalties, true);
-			println!("");
-			print_result(&layout, &penalty);
-		}
+        // Print the top layouts.
+        for entry in best_layouts.iter() {
+            let ref layout = entry.layout;
+            let penalty = penalty::calculate_penalty(&quartads, len, &layout, penalties, true);
+            println!("");
+            print_result(&layout, &penalty);
+        }
 
-		// Keep going until swapping doesn't get us any more improvements.
-		let best = best_layouts.pop_front().unwrap();
-		if curr_penalty <= best.penalty {
-			break;
-		} else {
-			curr_layout = best.layout;
-			curr_penalty = best.penalty;
-		}
-	}
+        // Keep going until swapping doesn't get us any more improvements.
+        let best = best_layouts.pop_front().unwrap();
+        if curr_penalty <= best.penalty {
+            break;
+        } else {
+            curr_layout = best.layout;
+            curr_penalty = best.penalty;
+        }
+    }
 
-	println!("");
-	println!("Ultimate winner:");
-	println!("{}", curr_layout);
+    println!("");
+    println!("Ultimate winner:");
+    println!("{}", curr_layout);
 }
 
 pub fn print_result<'a>(
-	layout: &'a layout::Layout,
-	penalty: &'a (f64, f64, Vec<penalty::KeyPenaltyResult<'a>>))
-{
-	println!("{}", layout);
+    layout: &'a layout::Layout,
+    penalty: &'a (f64, f64, Vec<penalty::KeyPenaltyResult<'a>>),
+) {
+    println!("{}", layout);
 
-	let (ref total, ref scaled, ref penalties) = *penalty;
-	println!("total: {}; scaled: {}", total, scaled);
-	for penalty in penalties {
-		print!("{}  / ", penalty);
-		let mut high_keys: Vec<(&str, f64)> = penalty.high_keys.iter().map(|x| (*x.0, *x.1)).collect();
-		high_keys.sort_by(|a, b|
-			match b.1.abs().partial_cmp(&a.1.abs()) {
-				Some(c) => c,
-				None => Ordering::Equal
-			});
-		for key in high_keys.iter().take(5) {
-			let (k, v) = *key;
-			print!(" {}: {};", k, v);
-		}
-		println!("");
-	}
+    let (ref total, ref scaled, ref penalties) = *penalty;
+    println!("total: {}; scaled: {}", total, scaled);
+    for penalty in penalties {
+        print!("{}  / ", penalty);
+        let mut high_keys: Vec<(&str, f64)> =
+            penalty.high_keys.iter().map(|x| (*x.0, *x.1)).collect();
+        high_keys.sort_by(|a, b| match b.1.abs().partial_cmp(&a.1.abs()) {
+            Some(c) => c,
+            None => Ordering::Equal,
+        });
+        for key in high_keys.iter().take(5) {
+            let (k, v) = *key;
+            print!(" {}: {};", k, v);
+        }
+        println!("");
+    }
 }
 
 // Take ownership of the list and give it back as a hack to make the borrow checker happy :^)
-fn list_insert_ordered(mut list: LinkedList<BestLayoutsEntry>, entry: BestLayoutsEntry)
--> LinkedList<BestLayoutsEntry>
-{
-	{
-		// Find where to add our new entry to, since the list is sorted.
-		let mut iter = list.iter_mut();
-		loop {
-			{
-				let opt_next = iter.peek_next();
-				if let Some(next) = opt_next {
-					let cmp = entry.cmp(next);
-					if cmp == Ordering::Less {
-						break;
-					}
-				} else {
-					break;
-				}
-			}
+fn list_insert_ordered(
+    mut list: LinkedList<BestLayoutsEntry>,
+    entry: BestLayoutsEntry,
+) -> LinkedList<BestLayoutsEntry> {
+    {
+        // Find where to add our new entry to, since the list is sorted.
+        let mut cursor = list.cursor_front_mut();
+        loop {
+            {
+                let opt_next = cursor.peek_next();
+                if let Some(next) = opt_next {
+                    let cmp = entry.cmp(next);
+                    if cmp == Ordering::Less {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
 
-			iter.next();
-		}
+            cursor.move_next();
+        }
 
-		// Add to list.
-		iter.insert_next(entry);
-	}
-	list
+        // Add to list.
+        cursor.insert_after(entry);
+    }
+    list
 }
